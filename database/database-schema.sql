834e78f6d506c759e8e57f59e0232c391138b5df
Martin Bernard <nawm@carleton.edu>
2021-01-26 18:42:12 -0800

    Cleaned up database-schema.sql

--- database/database-schema.sql
+++ database/database-schema.sql
@@ -6,13 +6,11 @@ CREATE TABLE Athletes (
     athlete_weight text,
     sport text
 );
-\copy athletes from 'Athletes.csv' DELIMITER ',' CSV NULL AS 'NULL'
 
 CREATE TABLE NOCs (
     id text,
     noc_abbre text
 );
-\copy nocs from 'NOCs.csv' DELIMITER ',' CSV NULL AS 'NULL'
 
 CREATE TABLE Medals (
     id text,
@@ -20,22 +18,18 @@ CREATE TABLE Medals (
     sport text,
     medal_type text
 );
-\copy medals from 'Medals.csv' DELIMITER ',' CSV NULL AS 'NULL'
 
 CREATE TABLE NOCs_Medals (
     noc_id text,
     medal_id text
 );
-\copy nocs_medals from 'NOCs_Medals.csv' DELIMITER ',' CSV NULL AS 'NULL'
 
 CREATE TABLE Athletes_NOCs (
     athlete_id text,
     noc_id text
 );
-\copy athletes_nocs from 'Athletes_NOCs.csv' DELIMITER ',' CSV NULL AS 'NULL'
 
 CREATE TABLE Athletes_Medals (
     athlete_id text,
     medal_id text
 );
-\copy athletes_medals from 'Athletes_Medals.csv' DELIMITER ',' CSV NULL AS 'NULL'
